17a0e709bddab62b8892ef7363319a450600b95c
Mitar <mitar.git@tnode.com>
2014-07-03 02:55:21 -0700

    Set hostname.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/bash -e
 
-docker run -d -p 80:80/tcp --name web -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/web
+docker run -d -p 80:80/tcp --name web -h web.web.server2.docker -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/web
