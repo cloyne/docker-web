69b41c9459407bc19d364ac714aeede71b044f8a
Mitar <mitar.git@tnode.com>
2014-07-06 20:29:15 -0700

    Prepare directories.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #/bin/bash -e
 
+mkdir -p /srv/var/log/web
+mkdir -p /srv/var/log/dockergen
+chown nobody:nogroup /srv/var/log/dockergen
+
 docker run -d -p 80:80/tcp --name web -h web.web.server2.docker -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/web
