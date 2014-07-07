01cd3cecba2acdac5e0b1c75092bb42249eadc44
Mitar <mitar.git@tnode.com>
2014-07-06 20:42:37 -0700

    Chown inside container.

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 mkdir -p /srv/var/log/web
 mkdir -p /srv/var/log/dockergen
-chown nobody:nogroup /srv/var/log/dockergen
 
 docker run -d -p 80:80/tcp --name web -h web.web.server2.docker -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/web
