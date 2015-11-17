46f329b3e763264919d5e1d013ad7c35d7bf4721
Mitar <mitar.git@tnode.com>
2015-11-17 12:12:19 -0800

    Mount Council storage into the web container.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 mkdir -p /srv/var/log/web
 mkdir -p /srv/var/log/dockergen
 
-docker run -d --restart=always -p 80:80/tcp --name web --hostname web -v /srv/var/hosts:/etc/hosts:ro -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/web
+docker run -d --restart=always -p 80:80/tcp --name web --hostname web -v /srv/var/hosts:/etc/hosts:ro -v /var/run/docker.sock:/docker.sock -v /srv/var/log/web:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen -v /srv/council/meteor/storage:/council-storage cloyne/web
